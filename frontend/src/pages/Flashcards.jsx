d20667ea7ed6ade6824640e136f79c35fc62b70d
Leroy-Hong <69704784+Leroy-Hong@users.noreply.github.com>
2024-07-14 05:42:48 +0800

    fix formatting

--- frontend/src/pages/Flashcards.jsx
+++ frontend/src/pages/Flashcards.jsx
@@ -16,76 +16,78 @@ import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import IconButton from '@mui/material/IconButton';
 
 export default function Flashcards() {
-  const {session, supabase} = useContext(authContext);
-	const [flashcards, setflashcards]=useState([{'question':"getting flashcards...",'answer':'getting flashcards...'}]);
-	const [current, setcurrent]=useState(0);
-	const [name,setname]=useState('Flashcards');
-	const [show, setshow]=useState(false);
+	const { session, supabase } = useContext(authContext);
+	const [flashcards, setflashcards] = useState([{ 'question': "getting flashcards...", 'answer': 'getting flashcards...' }]);
+	const [current, setcurrent] = useState(0);
+	const [name, setname] = useState('Flashcards');
+	const [show, setshow] = useState(false);
 	const [searchParams, setSearchParams] = useSearchParams();
-	useEffect(()=>{
-		fetch("http://localhost:5000/flashcards/"+searchParams.get("id"),{
-			  mode: 'cors',
-			  headers: {
-			    'Access-Control-Allow-Origin':'*'
-		  }
-		}).then(response=>{
-			if (!response.ok){
+	useEffect(() => {
+		fetch("http://localhost:5000/flashcards/" + searchParams.get("id"), {
+			mode: 'cors',
+			headers: {
+				'Access-Control-Allow-Origin': '*'
+			}
+		}).then(response => {
+			if (!response.ok) {
 				throw new Error('Erm WHAT THE SIGMA')
 			}
 			return response.json()
-		}).then(data=>{
+		}).then(data => {
 			console.log(data)
 			setflashcards(data[0]["flashcards_json"])
 			setname(data[0]["flashcard_name"])
 			console.log(data[0]["flashcards_json"])
 		})
-	},[])
-  
-  return (
-	<div style={{height:"100vh", width:"100vw", display:"flex", flexDirection:"column"}}>
+	}, [])
 
-		<HeaderBar/>
-	<Typography variant="h2" sx={{fontWeight:"bold"}}>{name} </Typography>
-		
-	  <Card className={`flip-container ${show?"flipped":""}`} sx={{height:"75%", m:12, marginBottom:5}}>
-		<CardActionArea className={`flipper`} sx={{height: "100%"}} onClick={()=>{setshow(!show)}}>
-		<CardContent className={`front`} sx={{height: "100%", width:"100%", justifyContent: 'center', bgcolor: 'primary.main', color: 'primary.contrastText'}}>
-	  		<Box sx={{display:"flex", height: "100%", justifyContent: 'center'}}>
-			  <Typography variant="h3" sx={{my:"auto"}}  component="div" align="center"  >
-{flashcards[current]["question"]}
+	return (
+		<div style={{ height: "100vh", width: "100vw", display: "flex", flexDirection: "column" }}>
 
-		      </Typography> 
-	  		</Box>
-	  	</CardContent>
-		<CardContent className={`back`} sx={{height: "100%",width:"100%", justifyContent: 'center', bgcolor: 'secondary.main', color: 'primary.contrastText'}}>
-	  		<Box sx={{display:"flex", height: "100%", justifyContent: 'center'}}>
-			  <Typography variant="h3" sx={{my:"auto"}}  component="div" align="center"  >
-		  {flashcards[current]["answer"]}
-		    </Typography> 
-	  		</Box>
-	  	</CardContent>
-	  	</CardActionArea>
-	  </Card>
-	  <Box sx={{display:"flex", flexGrow:"1", alignItems:"center", justifyContent:'center'}}>
-			<IconButton tooltip="Previous Flashcard"onClick={()=>{
-				if (current>0){
-				setcurrent(current-1)
-				setshow(false)
-				}}
-			}>
-	  		<NavigateBeforeIcon fontSize="large"/>
-			</IconButton>
-	  		<Typography variant="h5" sx={{mx: "1rem"}}>{`${current+1}/${flashcards.length}`} 
-	  		</Typography>
-			<IconButton tooltip="Next Flashcard" onClick={()=>{
-				if (current<flashcards.length-1){
-				setcurrent(current+1)
-				setshow(false)
-				}}
-			}>
-				<NavigateNextIcon fontSize="large" />
-			</IconButton>
-	  </Box>
-  </div>
-  )
+			<HeaderBar />
+			<Typography variant="h2" sx={{ fontWeight: "bold" }}>{name} </Typography>
+
+			<Card className={`flip-container ${show ? "flipped" : ""}`} sx={{ height: "75%", m: 12, marginBottom: 5 }}>
+				<CardActionArea className={`flipper`} sx={{ height: "100%" }} onClick={() => { setshow(!show) }}>
+					<CardContent className={`front`} sx={{ height: "100%", width: "100%", justifyContent: 'center', bgcolor: 'primary.main', color: 'primary.contrastText' }}>
+						<Box sx={{ display: "flex", height: "100%", justifyContent: 'center' }}>
+							<Typography variant="h3" sx={{ my: "auto" }} component="div" align="center"  >
+								{flashcards[current]["question"]}
+
+							</Typography>
+						</Box>
+					</CardContent>
+					<CardContent className={`back`} sx={{ height: "100%", width: "100%", justifyContent: 'center', bgcolor: 'secondary.main', color: 'primary.contrastText' }}>
+						<Box sx={{ display: "flex", height: "100%", justifyContent: 'center' }}>
+							<Typography variant="h3" sx={{ my: "auto" }} component="div" align="center"  >
+								{flashcards[current]["answer"]}
+							</Typography>
+						</Box>
+					</CardContent>
+				</CardActionArea>
+			</Card>
+			<Box sx={{ display: "flex", flexGrow: "1", alignItems: "center", justifyContent: 'center' }}>
+				<IconButton tooltip="Previous Flashcard" onClick={() => {
+					if (current > 0) {
+						setcurrent(current - 1)
+						setshow(false)
+					}
+				}
+				}>
+					<NavigateBeforeIcon fontSize="large" />
+				</IconButton>
+				<Typography variant="h5" sx={{ mx: "1rem" }}>{`${current + 1}/${flashcards.length}`}
+				</Typography>
+				<IconButton tooltip="Next Flashcard" onClick={() => {
+					if (current < flashcards.length - 1) {
+						setcurrent(current + 1)
+						setshow(false)
+					}
+				}
+				}>
+					<NavigateNextIcon fontSize="large" />
+				</IconButton>
+			</Box>
+		</div>
+	)
 }
