909d9a17551e58892335ce65dc8ca9ee1a6a91f1
jer123se12 <jerickseng06@gmail.com>
2024-07-14 05:29:41 +0800

    added things

--- frontend/src/pages/CreateFlashcard.jsx
+++ frontend/src/pages/CreateFlashcard.jsx
@@ -7,8 +7,15 @@ import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import { useContext } from 'react';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import { Navigate } from 'react-router-dom';
 import { authContext } from '../utils/authContext';
 import TextField from '@mui/material/TextField';
+import LoadingButton from '@mui/lab/LoadingButton';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 const VisuallyHiddenInput = styled('input')({
     clip: 'rect(0 0 0 0)',
@@ -25,7 +32,12 @@ const VisuallyHiddenInput = styled('input')({
 export default function CreateFlashcard() {
     const {session, supabase} = useContext(authContext);
     const [file, setFile] = useState(null);
+    const [loading, setloading] = useState(false);
+    const [redirect, setredirect] = useState(false);
     const [flashcardName, setFlashCardName] = useState(null);
+    const [open, setOpen] = React.useState(false);
+    const [page, setPage] = React.useState("/home");
+    const [id, setid] = React.useState(1);
 
     const handleFileChange = (e) => {
         if (e.target.files) {
@@ -35,11 +47,12 @@ export default function CreateFlashcard() {
     };
 
     async function submitFile() {
+
+	setloading(true)
         const formData = new FormData();
         formData.append('file', file);
         formData.append('user_id', session.user.id)
         formData.append('flashcard_name', flashcardName)
-        
         fetch('http://localhost:5000/extract-text', {
             method: 'POST',
             mode: 'cors',
@@ -48,9 +61,15 @@ export default function CreateFlashcard() {
 		    },
             body: formData
         }).then(response => {
-            console.log(response)
+	    return response.json()
 
-        })
+        }).then(response=>{
+            console.log(response)
+	    setloading(false)
+	    setid(response["flashcards_supabase_response"][0]["id"])
+	    setOpen(true)
+		
+		})
     }
 
     return (
@@ -69,10 +88,34 @@ export default function CreateFlashcard() {
                     : <></>
                 }
                 {flashcardName 
-                    ? <Button variant="contained" onClick={submitFile}>Go!</Button>
+                    ? <LoadingButton loading={loading}  variant="contained" onClick={submitFile}>Go!</LoadingButton>
                     : <></>
                 }
             </Stack>
+		{(redirect)?
+			<Navigate to={page}/>:""
+		}
+		<Dialog
+        open={open}
+        onClose={()=>{setredirect(true)}}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"File Created!"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Would you like to return home or view the new deck 
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={()=>{setredirect(true)}}>Return Home</Button>
+          <Button onClick={()=>{setPage(`/flashcards?id=${id}`);setredirect(true)}} autoFocus>
+		View the Deck
+          </Button>
+        </DialogActions>
+      </Dialog>
         </Box>
     )
 }
