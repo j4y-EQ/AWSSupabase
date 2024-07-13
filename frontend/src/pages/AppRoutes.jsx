fa943437ea7b402328b7b4f193d94fbd589bea0f
Leroy-Hong <69704784+Leroy-Hong@users.noreply.github.com>
2024-07-13 22:47:29 +0800

    fixed routing

--- frontend/src/pages/AppRoutes.jsx
+++ frontend/src/pages/AppRoutes.jsx
@@ -1,12 +1,15 @@
 import React, { useContext } from 'react'
 import { Routes, Navigate, Route } from 'react-router-dom';
 import Home from './Home';
+import Flashcards from './Flashcards';
 
 const AppRoutes = () => {
     return (    
         <Routes>
             <Route path='/' element={<Navigate to={'/home'}/>} />
             <Route path='*' element={<Navigate to={'/home'}/>} />
+            
+            <Route path='/flashcards' element={<Flashcards/>} />
             <Route path='/home' element={<Home/>} />
         </Routes>
     )
